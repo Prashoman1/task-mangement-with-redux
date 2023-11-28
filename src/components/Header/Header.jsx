22e4eb4812978a7cede26566c38d783c9749b886
razibul <razibul15-11649@diu.edu.bd>
2023-11-28 18:28:50 +0600

    design done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,28 @@
-import React, { useState } from 'react'
-import Modal from '../../utiles/Modal'
+import React, { useState } from "react";
+import Modal from "../../utiles/Modal";
 
 export default function Header() {
-  const [modalSate, setModalState] = useState('');
+  const [modalSate, setModalState] = useState("");
   return (
     <>
-    <div className="navbar bg-sky-200">
-  <div className="flex-1">
-    <a className="btn btn-ghost normal-case text-xl">Demu Redux Project</a>
-  </div>
-  <div className="flex-none">
-    <div>
-        <button  className='btn btn-sm btn-primary'>Add Task</button>
-    </div>
-  </div>
-</div>
-  <Modal></Modal>
+      <div className="navbar bg-sky-200">
+        <div className="flex-1">
+          <a className="btn btn-ghost normal-case text-xl">
+            Demu Redux Project
+          </a>
+        </div>
+        <div className="flex-none">
+          <div>
+            <button
+              className="btn btn-sm btn-primary"
+              onClick={() => document.getElementById("product_add").showModal()}
+            >
+              Add Task
+            </button>
+          </div>
+        </div>
+      </div>
+      <Modal></Modal>
     </>
-  )
+  );
 }
--- src/utiles/Modal.jsx
+++ src/utiles/Modal.jsx
@@ -1,20 +1,31 @@
 import React from 'react'
 
 export default function Modal() {
+
+  const handleModalClose = (e,modalId)=>{
+    e.preventDefault();
+    document.getElementById(modalId).close();
+  }
   return (
     <>
-    {/* Open the modal using document.getElementById('ID').showModal() method */}
-    <button className="btn" onClick={()=>document.getElementById('my_modal_1').showModal()}>open modal</button>
-    <dialog id="my_modal_1" className="modal">
+  
+    <dialog id="product_add" className="modal">
     <div className="modal-box">
-        <h3 className="font-bold text-lg">Hello!</h3>
-        <p className="py-4">Press ESC key or click the button below to close</p>
-        <div className="modal-action">
-        <form method="dialog">
-            {/* if there is a button in form, it will close the modal */}
-            <button className="btn">Close</button>
+        <form action="">
+          <div className='py-2'>
+            <label>Tittle</label>
+            <input type="text" name='title' className="w-full border border-blue-300 focus:outline-none py-2 px-4 rounded" placeholder="Tittle" />
+          </div>
+          <div className='py-2'>
+            <label>details</label>
+            
+            <textarea name="details" id="" cols="5"  className="w-full border border-blue-300 focus:outline-none py-2 px-4 rounded"></textarea>
+          </div>
+          <div className='flex items-center justify-end'>
+            <button onClick={(e)=>handleModalClose(e,'product_add')} className='btn btn-sm btn-danger'>close</button>
+            <button className='btn btn-sm btn-success ms-3'>submit</button>
+          </div>
         </form>
-        </div>
     </div>
     </dialog>
     </>
